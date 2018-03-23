cc6d2704fe2cab838fbbc29302023a85f4625165
Arko Chatterjee <arkochatterjee@gmail.com>
2018-03-23 21:33:36 +0530

    RFID Module Integrated!

--- nodemcu-wifi.ino
+++ nodemcu-wifi.ino
@@ -1,5 +1,6 @@
  #include <ESP8266WiFi.h>
-
+char input[12];
+int count=0;
 const char* ssid     = "baal chero";
 const char* password = "arko1234";
 
@@ -7,32 +8,34 @@ int wifiStatus;
 
 void setup() {
   
-  Serial.begin(115200);
-  delay(200);
+  Serial.begin(9600);
+ // delay(200);
   
    WiFi.begin(ssid, password);
       
-      while (WiFi.status() != WL_CONNECTED) {
+    /*  while (WiFi.status() != WL_CONNECTED) {
         delay(500);
         Serial.print(".");
-      }
+      }*/
 }
 
 void loop() {
   
-   wifiStatus = WiFi.status();
+   if(Serial.available())
+  {
+    count=0;
+    while(Serial.available()&& count<12)
+    {
+      input[count]=Serial.read();
+      count++;
+      delay(5);
+    }
+    Serial.println("Data Read :");
+    for(int i=0;i<12;i++)
+    Serial.print(input[i]); 
 
-      if(wifiStatus == WL_CONNECTED){
-         Serial.println("");
-         Serial.println("Your ESP is connected!");  
-         Serial.println("Your IP address is: ");
-         Serial.println(WiFi.localIP());  
-      }
-      else{
-        Serial.println("");
-        Serial.println("WiFi not connected");
-      }
-      delay(1000); 
-
+    Serial.println();
+  }
+  
 
 }
